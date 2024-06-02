37ebf0a421c7f0682b4ac34262673c9cfe7dd40b
jwfraustro <36318163+jwfraustro@users.noreply.github.com>
2024-06-02 14:34:33 -0400

    basic cache, joblist, create, get

---
+++ uws/generated/servers/python/fastapi/src/impl/cache/uws_cache.py
@@ -0,0 +1,82 @@
+"""Example in-memory UWS job cache implementation."""
+import time
+from datetime import datetime
+from typing import List, Optional, Union
+
+from uws_server.models.execution_phase import ExecutionPhase
+from uws_server.models.job_summary import JobSummary
+from uws_server.models.jobs import Jobs
+from uws_server.models.short_job_description import ShortJobDescription
+
+
+class UWSCache:
+    """Example in-memory UWS job cache implementation."""
+
+    def __init__(self):
+        self.jobs: dict[str, dict] = {}
+
+    def get_job(self, job_id: str, phase: ExecutionPhase = None, wait: Optional[int] = None) -> Union[JobSummary, None]:
+        """Get a job from the cache."""
+
+        job_dict = self.jobs.get(job_id, None)
+
+        if job_dict:
+            job_summary = JobSummary(**job_dict)
+        else:
+            return None
+
+        # phase and wait are used for polling phase changes
+        # here, we just mock it by sleeping
+
+        if phase or wait:
+            if not wait:
+                wait = 5
+            if not phase:
+                phase = ExecutionPhase.EXECUTING
+            time.sleep(wait)
+            job_summary.phase = phase
+            self.update_job(job_id, job_summary.to_dict())
+
+        return job_summary
+
+    def get_jobs(self, phase: List[ExecutionPhase] = [], after: Optional[str] = None, last: Optional[int] = None):
+        """Get all jobs from the cache."""
+
+        all_jobs = []
+
+        # Get last n jobs - gets all jobs otherwise
+        all_jobs = [ShortJobDescription(id=job_id, **job_dict) for job_id, job_dict in self.jobs.items()]
+        all_jobs.sort(key=lambda job: job.creation_time, reverse=True)
+
+        if last:
+            all_jobs = all_jobs[:last]
+
+        # Filter by phase
+        if phase:
+            all_jobs = [job for job in all_jobs if job.phase in phase]
+
+        # Filter by after
+        if after:
+            all_jobs = [job for job in all_jobs if job.creation_time > datetime.fromisoformat(after)]
+
+        job_list = Jobs(jobref=all_jobs)
+
+        return job_list
+
+    def add_job(self, job_id: str, job: dict):
+        """Add a job to the cache."""
+
+        self.jobs[job_id] = job
+
+    def update_job(self, job_id: str, job: dict):
+        """Update a job in the cache."""
+        self.add_job(job_id, job)
+
+    def delete_job(self, job_id: str):
+        """Delete a job from the cache."""
+
+        del self.jobs[job_id]
+
+    def clear(self):
+        """Clear the cache."""
+        self.jobs.clear()
--- uws/generated/servers/python/fastapi/src/impl/uws_api_impl.py
+++ uws/generated/servers/python/fastapi/src/impl/uws_api_impl.py
@@ -1,9 +1,142 @@
-from typing import List
+from datetime import datetime
+from typing import List, Optional
+from uuid import uuid4
+
+from fastapi.responses import JSONResponse
 from uws_server.apis.uws_api_base import BaseUWSApi
+from uws_server.models.error_summary import ErrorSummary
 from uws_server.models.execution_phase import ExecutionPhase
+from uws_server.models.job_summary import JobSummary
 from uws_server.models.jobs import Jobs
+from uws_server.models.parameters import Parameters
+from uws_server.models.post_update_job_destruction_request import PostUpdateJobDestructionRequest
+from uws_server.models.post_update_job_execution_duration_request import PostUpdateJobExecutionDurationRequest
+from uws_server.models.post_update_job_parameters_request import PostUpdateJobParametersRequest
+from uws_server.models.post_update_job_phase_request import PostUpdateJobPhaseRequest
+from uws_server.models.post_update_job_request import PostUpdateJobRequest
+from uws_server.models.results import Results
+
+from impl.cache.uws_cache import UWSCache
+
+job_cache = UWSCache()
+
 
 class UWSAPIImpl(BaseUWSApi):
+    def get_job_list(
+        self, phase: List[ExecutionPhase] = [], after: Optional[str] = None, last: Optional[int] = None
+    ) -> Jobs:
+        return job_cache.get_jobs(phase, after, last)
 
-    def get_job_list(self, phase: List[ExecutionPhase], after: str, last: int) -> Jobs:
-        return Jobs(jobref=[{"id":"example"}])
+    def delete_job(
+        self,
+        job_id: str,
+    ) -> Jobs:
+        if not job_cache.get_job(job_id):
+            return JSONResponse(status_code=404, content={"message": "Job not found"})
+
+        job_cache.delete_job(job_id)
+
+    def get_job_destruction(
+        self,
+        job_id: str,
+    ) -> datetime:
+        ...
+
+    def get_job_error_summary(
+        self,
+        job_id: str,
+    ) -> ErrorSummary:
+        ...
+
+    def get_job_execution_duration(
+        self,
+        job_id: str,
+    ) -> int:
+        ...
+
+    def get_job_owner(
+        self,
+        job_id: str,
+    ) -> str:
+        ...
+
+    def get_job_parameters(
+        self,
+        job_id: str,
+    ) -> Parameters:
+        ...
+
+    def get_job_phase(
+        self,
+        job_id: str,
+    ) -> ExecutionPhase:
+        ...
+
+    def get_job_quote(
+        self,
+        job_id: str,
+    ) -> datetime:
+        ...
+
+    def get_job_results(
+        self,
+        job_id: str,
+    ) -> Results:
+        ...
+
+    def get_job_summary(
+        self,
+        job_id: str,
+        phase: str,
+        wait: int,
+    ) -> JobSummary:
+        return job_cache.get_job(job_id, phase, wait)
+
+    def post_create_job(
+        self,
+        parameters: Parameters,
+    ) -> None:
+        job_id = str(uuid4())
+        job = JobSummary(
+            job_id=job_id,
+            creationTime=datetime.now().isoformat(),
+            phase=ExecutionPhase.PENDING,
+            parameters=parameters,
+        )
+        job_cache.add_job(job_id, job.to_dict())
+        return self.get_job_summary(job_id, None, None)
+
+    def post_update_job(
+        self,
+        job_id: str,
+        post_update_job_request: PostUpdateJobRequest,
+    ) -> datetime:
+        ...
+
+    def post_update_job_destruction(
+        self,
+        job_id: str,
+        post_update_job_destruction_request: PostUpdateJobDestructionRequest,
+    ) -> None:
+        ...
+
+    def post_update_job_execution_duration(
+        self,
+        job_id: str,
+        post_update_job_execution_duration_request: PostUpdateJobExecutionDurationRequest,
+    ) -> None:
+        ...
+
+    def post_update_job_parameters(
+        self,
+        job_id: str,
+        post_update_job_parameters_request: PostUpdateJobParametersRequest,
+    ) -> JobSummary:
+        ...
+
+    def post_update_job_phase(
+        self,
+        job_id: str,
+        post_update_job_phase_request: PostUpdateJobPhaseRequest,
+    ) -> None:
+        ...
